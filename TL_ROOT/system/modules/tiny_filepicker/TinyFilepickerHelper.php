094049515d7dc6e1b9a64d9efc1353bdf8ff416b
Oliver Lohoff <oliverlohoff@googlemail.com>
2013-02-06 20:04:34 +0100

    changed some path urls

--- TL_ROOT/system/modules/tiny_filepicker/TinyFilepickerHelper.php
+++ TL_ROOT/system/modules/tiny_filepicker/TinyFilepickerHelper.php
@@ -1,12 +1,13 @@
-<?php if(!defined('TL_ROOT')) {die('You cannot access this file directly!');
-}
+<?php 
+
+namespace c4y;
 
 /**
  * @copyright 4ward.media 2012 <http://www.4wardmedia.de>
  * @author Christoph Wiechert <wio@psitrax.de>
  */
  
-class TinyFilepickerHelper extends System
+class TinyFilepickerHelper extends \System
 {
 
 	public function TinyMCE_Customzier_getFilebrowser($arrBrowsers)
@@ -15,7 +16,7 @@ class TinyFilepickerHelper extends System
 		(
 			'label'	=> 'Tiny Filebrowser',
 			'javascript' => '
-    fileBrowserURL = "'.TL_PATH.'/system/modules/tiny_filepicker/tinyfiles.php";
+    fileBrowserURL = "'.TL_PATH.'/contao/tinyfiles.php";
     tinyMCE.activeEditor.windowManager.open({
             title: "Contao File Browser",
             url: fileBrowserURL,
---
+++ TL_ROOT/system/modules/tiny_filepicker/config/autoload.php
@@ -0,0 +1,21 @@
+<?php
+
+
+/**
+ * Register the namespaces
+ */
+ClassLoader::addNamespaces(array
+(
+    'c4y',
+));
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+    // Classes
+    'c4y\TinyFilepickerHelper'   => 'system/modules/tiny_filepicker/TinyFilepickerHelper.php',
+
+));
